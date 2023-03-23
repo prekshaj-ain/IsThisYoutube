b9b1db6cb6fe1a9fb446863033393e04c11ade9d
preksha <jainpreksha2712@gmail.com>
2023-03-24 01:38:18 +0530

    added detectClickOutside component

---
+++ src/Components/DetectClickOutside.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useRef } from 'react'
+
+const DetectClickOutside = (props) => {
+    const {onClickOutside,children} = props;
+    const ref = useRef(null);
+    const handleClickEvent = (e)=>{
+        if(ref.current && !ref.current.contains(e.target)){
+            onClickOutside && onClickOutside();
+        }
+    }
+    useEffect(()=>{
+        document.addEventListener("click",handleClickEvent,true)
+        return ()=>document.removeEventListener("click",handleClickEvent,true)
+    },[])
+    if(!children) return null;
+  return (
+    <div ref={ref}>{children}</div>
+  )
+}
+
+export default DetectClickOutside
