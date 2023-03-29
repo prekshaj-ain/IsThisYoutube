346dc8142b1c8d0ceaeea2692040524c801211c9
preksha <jainpreksha2712@gmail.com>
2023-03-29 23:07:11 +0530

    changed constants

--- src/Utils/Constants.js
+++ src/Utils/Constants.js
@@ -4,6 +4,6 @@ export const YOUTUBE_VIDEO_URL = `https://youtube.googleapis.com/youtube/v3/vide
 export const YOUTUBE_SUGGESTION_URL = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&type=video&key=${process.env.REACT_APP_API_KEY}&relatedToVideoId=`;
 
 export const YOUTUBE_TAG_URL =  `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&type=video&key=${process.env.REACT_APP_API_KEY}&q=`
-export const YOUTUBE_SEARCH_URL =  `http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=`;
+export const YOUTUBE_SEARCH_URL =  `https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=`;
 export const YOUTUBE_CHANNEL_URL = `https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&key=${process.env.REACT_APP_API_KEY}&id=`
 export const LIVE_CHAT_COUNT = 100; 
