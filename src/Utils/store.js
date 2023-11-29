e25c57f64d069be21d4aa0766411fb6ba09f6fc6
preksha <jainpreksha2712@gmail.com>
2023-11-29 11:27:18 +0530

    :up: add comment Slice

--- src/Utils/store.js
+++ src/Utils/store.js
@@ -6,17 +6,19 @@ import relatedVideosSlice from "./relatedVideosSlice";
 import searchSlice from "./searchSlice";
 import searchVideosSlice from "./searchVideosSlice";
 import selectedVideoSlice from "./selectedVideoSlice";
+import CommentSlice from "./CommentSlice";
 
 const store = configureStore({
-    reducer:{
-        app: appSlice,
-        search: searchSlice,
-        homeVideo : homeVideoSlice,
-        selectedVideo : selectedVideoSlice,
-        relatedVideo: relatedVideosSlice,
-        searchVideo: searchVideosSlice,
-        chat: chatSlice,
-    }
+  reducer: {
+    app: appSlice,
+    search: searchSlice,
+    homeVideo: homeVideoSlice,
+    selectedVideo: selectedVideoSlice,
+    relatedVideo: relatedVideosSlice,
+    searchVideo: searchVideosSlice,
+    chat: chatSlice,
+    Comments: CommentSlice,
+  },
 });
 
 export default store;
